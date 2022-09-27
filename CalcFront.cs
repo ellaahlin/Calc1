946a2b7f19738bc6e800db2238321c874ac956a3
ellaahlin <111963453+ellaahlin@users.noreply.github.com>
2022-09-27 14:02:27 +0200

    Add files via upload

--- CalcFront.cs
+++ CalcFront.cs
@@ -5,6 +5,8 @@ namespace Calc
 {
     public class CalcFront
     {
+        List<string> beräkningar = new List<string>();
+
         public void Run()
         {
             Welcome();
@@ -22,88 +24,110 @@ namespace Calc
         {
             WriteLine("1. Använd miniräknaren\n2. Se historik");
             int val = Convert.ToInt32(ReadLine());
+            string retry = "n";
 
-            switch (val)
+            do
             {
-                case 1:
-                    Clear();
-                    Write("Skriv in en beräkning: ");
-                    string beräkning = ReadLine();
-                    if (beräkning.Contains("+"))
-                    {
-                        int plus = beräkning.IndexOf("+");
-                        string temp1 = beräkning[..plus];
-                        string temp2 = beräkning[(plus + 1)..];
-                        int tal1 = Convert.ToInt32(temp1);
-                        int tal2 = Convert.ToInt32(temp2);
-                        int addition = tal1 + tal2;
-                        WriteLine($"{tal1} + {tal2} = {addition}");
-                        ReadLine();
-                    }
-                    else if (beräkning.Contains("-"))
-                    {
-                        int minus = beräkning.IndexOf("-");
-                        string temp1 = beräkning[..minus];
-                        string temp2 = beräkning[(minus + 1)..];
-                        int tal1 = Convert.ToInt32(temp1);
-                        int tal2 = Convert.ToInt32(temp2);
-                        int subtraktion = tal1 - tal2;
-                        WriteLine($"{tal1} - {tal2} = {subtraktion}");
-                        ReadLine();
-                    }
-                    else if (beräkning.Contains("*"))
-                    {
-                        int mult1 = beräkning.IndexOf("*");
-                        string temp1 = beräkning[..mult1];
-                        string temp2 = beräkning[(mult1 + 1)..];
-                        int tal1 = Convert.ToInt32(temp1);
-                        int tal2 = Convert.ToInt32(temp2);
-                        int multiplikation = (tal1 * tal2);
-                        WriteLine($"{tal1} * {tal2} = {multiplikation}");
-                        ReadLine();
-                    }
-                    else if (beräkning.Contains("x"))
-                    {
-                        int mult2 = beräkning.IndexOf("x");
-                        string temp1 = beräkning[..mult2];
-                        string temp2 = beräkning[(mult2 + 1)..];
-                        int tal1 = Convert.ToInt32(temp1);
-                        int tal2 = Convert.ToInt32(temp2);
-                        int multiplikation = (tal1 * tal2);
-                        WriteLine($"{tal1} x {tal2} = {multiplikation}");
-                        ReadLine();
-                    }
-                    else if (beräkning.Contains("/"))
-                    {
-                        int div = beräkning.IndexOf("/");
-                        string temp1 = beräkning[..div];
-                        string temp2 = beräkning[(div + 1)..];
-                        int tal1 = Convert.ToInt32(temp1);
-                        int tal2 = Convert.ToInt32(temp2);
-                        int division = (tal1 / tal2);
-                        WriteLine($"{tal1} / {tal2} = {division}");
+                switch (val)
+                {
+                    case 1:
+                        Clear();
+                        Write("Skriv in en beräkning: ");
+                        string beräkning = ReadLine();
+                        if (beräkning.Contains("+"))
+                        {
+                            int plus = beräkning.IndexOf("+");
+                            string temp1 = beräkning[..plus];
+                            string temp2 = beräkning[(plus + 1)..];
+                            int tal1 = Convert.ToInt32(temp1);
+                            int tal2 = Convert.ToInt32(temp2);
+                            int addition = tal1 + tal2;
+                            WriteLine($"{tal1} + {tal2} = {addition}");
+                            string resultat = $"{tal1} + {tal2} = {addition}";
+                            beräkningar.Add(resultat);
+                        }
+                        else if (beräkning.Contains("-"))
+                        {
+                            int minus = beräkning.IndexOf("-");
+                            string temp1 = beräkning[..minus];
+                            string temp2 = beräkning[(minus + 1)..];
+                            int tal1 = Convert.ToInt32(temp1);
+                            int tal2 = Convert.ToInt32(temp2);
+                            int subtraktion = tal1 - tal2;
+                            WriteLine($"{tal1} - {tal2} = {subtraktion}");
+                            string resultat = $"{tal1} - {tal2} = {subtraktion}";
+                            beräkningar.Add(resultat);
+                        }
+                        else if (beräkning.Contains("*"))
+                        {
+                            int mult1 = beräkning.IndexOf("*");
+                            string temp1 = beräkning[..mult1];
+                            string temp2 = beräkning[(mult1 + 1)..];
+                            int tal1 = Convert.ToInt32(temp1);
+                            int tal2 = Convert.ToInt32(temp2);
+                            int multiplikation = (tal1 * tal2);
+                            WriteLine($"{tal1} * {tal2} = {multiplikation}");
+                            string resultat = $"{tal1} * {tal2} = {multiplikation}";
+                            beräkningar.Add(resultat);
+                        }
+                        else if (beräkning.Contains("x"))
+                        {
+                            int mult2 = beräkning.IndexOf("x");
+                            string temp1 = beräkning[..mult2];
+                            string temp2 = beräkning[(mult2 + 1)..];
+                            int tal1 = Convert.ToInt32(temp1);
+                            int tal2 = Convert.ToInt32(temp2);
+                            int multiplikation = (tal1 * tal2);
+                            WriteLine($"{tal1} x {tal2} = {multiplikation}");
+                            string resultat = $"{tal1} x {tal2} = {multiplikation}";
+                            beräkningar.Add(resultat);
+                        }
+                        else if (beräkning.Contains("/"))
+                        {
+                            try
+                            {
+
+                                int div = beräkning.IndexOf("/");
+                                string temp1 = beräkning[..div];
+                                string temp2 = beräkning[(div + 1)..];
+                                int tal1 = Convert.ToInt32(temp1);
+                                int tal2 = Convert.ToInt32(temp2);
+                                int division = (tal1 / tal2);
+                                WriteLine($"{tal1} / {tal2} = {division}");
+                                string resultat = $"{tal1} / {tal2} = {division}";
+                                beräkningar.Add(resultat);
+                            }
+                            catch (DivideByZeroException e)
+                            {
+                                WriteLine("Division med noll inte möjligt.");
+                            }
+                            
+                        }
                         WriteLine("Vill du utföra en ny beräkning? (j/n): ");
-                        
-                        ReadLine();
-                    }
-                    break;
+                        retry = ReadLine();
+                        if (retry == "n")
+                        {
+                            Clear();
+                            Menu();
+                        }
+                        break;
 
-                case 2:
-                    Clear();
-                    break;
+                    case 2:
+                        Clear();
+                        foreach (string s in beräkningar)
+                        {
+                            WriteLine(s);
+                        }
+                        ReadKey();
+                        break;
 
-                default:
-                    Clear();
-                    Run();
-                    break;
-            }
+                    default:
+                        Clear();
+                        Run();
+                        break;
+                }
+            } while (retry != "n");
         }
-
-        private void History()
-        {
-
-        }
-
 
     }
 }
